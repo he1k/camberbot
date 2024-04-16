21e6f2741fc6fd0e698aeb0c8be8d06dddf0a42f
ulrik helk <ullehelk@gmail.com>
2024-04-16 13:03:37 +0200

    Update

--- model/matlab/motor.m
+++ model/matlab/motor.m
@@ -7,6 +7,12 @@ Kt = 31.82e-3;           % Torque constant
 Ke = 3.33*1e-3/(2*pi/60);% Back EMF constant
 n0 = 5400/60;            % No load rotor speed - Rotations per second
 n0*1/N_gbox              % No load speed of output shaft
-N_belt = 2;
+N_belt = 3;
+d = 0.15;
+o = pi*0.20;
 n0*1/N_gbox*N_belt
-n0*1/N_gbox*N_belt*pi*0.15
+n0*1/N_gbox*N_belt*o
+PPR = 5120;
+CPR = 4*PPR;
+res_mm = o/CPR*1e3
+
